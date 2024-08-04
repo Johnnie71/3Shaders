8b407cedc850c3c37af10b7d0def87294dc92fb6
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2024-08-04 16:42:32 -0400

    added: vite shader plugin and added to vite config

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import testVertexShader from './shaders/test/vertex.glsl'
 
 /**
  * Base
